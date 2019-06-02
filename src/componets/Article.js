5b03a9dcd21f6b543d36c2f0ac83b50114d57d72
vood <balitsky_igor@list.ru>
2019-06-02 16:55:00 +0300

    initial

--- src/componets/Article.js
+++ src/componets/Article.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Article(props ) {
-    const article = {props}
+    const {article} = props
     const body = <section>{article.text}</section>
     return (
         <div>
             <h2>{article.title}</h2>
             {body}
-            <h3>creation date: {(new Date()).toDateString()} </h3>
+            <h3>creation date: {(new Date(article.date)).toDateString()} </h3>
         </div>
     )
 }
